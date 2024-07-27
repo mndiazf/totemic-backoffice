0e7e940b1b9c9f78d2579fa92a98e802e6490077
mndiazf <mn.diaz.f@gmail.com>
2024-07-27 18:34:10 -0400

    header-nav-commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  @Input() isOpen = false;
+}
